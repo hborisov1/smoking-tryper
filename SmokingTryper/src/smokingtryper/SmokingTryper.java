cd4e6dbe3016a680906e2c5f3b891408d8d4451d
hborisov1 <hborisov1@gmail.com>
2012-12-21 17:41:12 +0200

    In master for 5th time

--- SmokingTryper/src/smokingtryper/SmokingTryper.java
+++ SmokingTryper/src/smokingtryper/SmokingTryper.java
@@ -6,7 +6,7 @@ public class SmokingTryper {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//in master again and again (basto) and again
+		//in master again and again (basto) and again and again
 	}
 
 }
