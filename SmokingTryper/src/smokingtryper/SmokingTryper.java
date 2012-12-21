c26056766f2d1a485eee52163e16e4e1c7e05dc1
hborisov1 <hborisov1@gmail.com>
2012-12-21 17:25:40 +0200

    In dev branch 4

--- SmokingTryper/src/smokingtryper/SmokingTryper.java
+++ SmokingTryper/src/smokingtryper/SmokingTryper.java
@@ -6,7 +6,7 @@ public class SmokingTryper {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		//in development branch again oookaaay
+		//in development branch again oookaaay and again
 	}
 
 }
